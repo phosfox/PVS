ad32571dc54ec7a052a1b4d7cb7c375362357817
Constantin <constantin.wentzek@student-fhws.de>
2018-03-19 19:27:20 +0100

    Summe mit Execserv gelöst

---
+++ src/ex_05/ExecutorService_Sum.java
@@ -0,0 +1,32 @@
+package ex_05;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ExecutorService_Sum {
+    public static void main(String[] args) throws Exception {
+        int N = 1000000;
+        int P = 100;
+        int piece = N / P;
+        int[] data = Sequential_Sum.initalizeArray(N);
+        ExecutorService exes = Executors.newFixedThreadPool(4);
+        List<Future> futures = new LinkedList<Future>();
+
+        for (int i = 0; i < P; i++) {
+            int start = i * piece;
+            int end = ((i + 1) * piece) - 1;
+             MaxWorker r = new MaxWorker(data, start, end);
+             Future<Integer> f = exes.submit(r);
+             futures.add(f);
+        }
+        Integer result = 0;
+        for (Future<Integer> f : futures) {
+            result += f.get();
+        }
+        System.out.println(result);
+        exes.shutdown();
+    }
+}
---
+++ src/ex_05/MaxWorker.java
@@ -0,0 +1,29 @@
+package ex_05;
+
+import java.util.concurrent.Callable;
+
+public class MaxWorker implements Callable<Integer> {
+    private int[]data;
+    private int start;
+    private int end;
+
+    public MaxWorker(int[] data, int start, int end){
+        this.data = data;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public Integer call() throws Exception{
+        return sum(this.data, this.start, this.end);
+    }
+
+    public int sum(int data[], int start, int end){
+        int sum = 0;
+        for (int i = start; i < end; i++) {
+            sum += data[i];
+        }
+
+        return sum;
+    }
+}
