bc6c23d37080e5224fc17ae94b903be10e3ae232
Constantin <constantin.wentzek@student-fhws.de>
2018-03-21 18:50:47 +0100

    ParallelStream gelöst????

---
+++ src/ex_05/ParallelStream_Sum.java
@@ -0,0 +1,22 @@
+package ex_05;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ParallelStream_Sum {
+
+    public static void main(String[] args) {
+        List random
+                = Collections.unmodifiableList(
+                new Random()
+                        .ints(0, 2000)
+                        .limit(2500)
+                        .boxed()
+                        .collect(Collectors.toList())
+        );
+
+        random.stream().forEach(System.out::println);
+        Object collect = random.parallelStream().collect(Collectors.summingInt(Integer::intValue));
+        System.out.println(collect);
+    }
+}
